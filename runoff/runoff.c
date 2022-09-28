328ab8d04b23800c89a62ddc23a80790b83fce65
Aaron-JH <62715885+Aaron-JH@users.noreply.github.com>
2022-09-28 11:12:34 -0700

    Update runoff.c

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -220,7 +220,7 @@ void eliminate(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == min)
+        if (candidates[i].votes <= min)
         {
             candidates[i].eliminated = true;
         }
